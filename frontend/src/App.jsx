108b43adbb12a47b48d39c0e82fa7b9184c1e44f
ritikjoshi22 <joshster.ritik@gmail.com>
2025-06-11 11:56:24 +0545

    Testing linux setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 const App = () => {
-  return (
-    <div><h1 class="text-3xl font-bold underline bg-red-700">
-    Hello world!
-  </h1></div>
-  )
+  return
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
 }
 
 export default App
