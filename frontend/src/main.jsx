edafc3f6bb2418ab0b4e894d116e18ddbd9c2537
ritikjoshi22 <joshster.ritik@gmail.com>
2025-07-11 18:14:41 +0545

    Connecting backend and frontend for login and signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,39 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Route, RouterProvider,createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
-import Roots from './Roots'
-import Home from './Component/Home/Home'
-import About from './Component/About'
-import Product from './Component/Product'
-import Contact from './Component/Contact'
-import Login from './Component/Auth/Login'
-import Dashboard from './Component/Dashboard' 
-import AuthLayout from './Authlayout'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Roots from "./Roots";
+import Home from "./Component/Home/Home";
+import About from "./Component/About";
+import Product from "./Component/Product";
+import Contact from "./Component/Contact";
+import Login from "./Component/Auth/Login";
+import Dashboard from "./Component/Dashboard";
+import AuthLayout from "./Authlayout";
 
-const router= createBrowserRouter(
-  createRoutesFromElements (
+const router = createBrowserRouter(
+  createRoutesFromElements(
     <>
-   <Route path='/' element={<Roots/>}>
-<Route path='' element={<Home/>} />
-<Route path='about' element={<About/>} />
-<Route path='products' element={<Product/>} />
-<Route path='contact' element={<Contact/>} />
-{/* <Route path='login' element={<Login/>} /> */}
-<Route path='dashboard' element={<Dashboard/>} />
-   </Route>
+      <Route path="/" element={<Roots />}>
+        <Route path="" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Product />} />
+        <Route path="contact" element={<Contact />} />
+        {/* <Route path='login' element={<Login/>} /> */}
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
 
-   <Route  element={<AuthLayout/>}>
-     <Route path='Login' element={<Login />} />
-   </Route>
+      <Route element={<AuthLayout />}>
+        <Route path="Login" element={<Login />} />
+      </Route>
     </>
   )
 );
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>
 );
