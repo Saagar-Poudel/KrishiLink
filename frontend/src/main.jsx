9268f61f88219658d65d6365e3d667a543cbf46f
Saagar-Poudel <poudelsagar920@gmail.com>
2025-07-29 15:33:03 +0545

    solving merge error

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,28 +8,10 @@ import WeatherAndPrices from './Component/WeatherAndPrices'
 import Marketplace from './Component/Marketplace'
 import NewsSection from './Component/NewsSection'
 import { LanguageProvider } from './contexts/LanguageContext'
-
 import Contact from './Component/Contact'
 import Login from './Component/Auth/Login'
-
 import AuthLayout from './Authlayout'
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Roots from "./Roots";
-import Home from "./Component/Home/Home";
-import About from "./Component/About";
-import Product from "./Component/Product";
-import Contact from "./Component/Contact";
-import Login from "./Component/Auth/Login";
-import Dashboard from "./Component/Dashboard";
-import AuthLayout from "./Authlayout";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -44,11 +26,6 @@ const router = createBrowserRouter(
    </Route>
       <Route path="/" element={<Roots />}>
         <Route path="" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="products" element={<Product />} />
-        <Route path="contact" element={<Contact />} />
-        {/* <Route path='login' element={<Login/>} /> */}
-        <Route path="dashboard" element={<Dashboard />} />
       </Route>
 
       <Route element={<AuthLayout />}>
@@ -61,10 +38,7 @@ const router = createBrowserRouter(
 createRoot(document.getElementById("root")).render(
   <StrictMode>
      <LanguageProvider>
-
-    
     <RouterProvider router={router}/>
     </LanguageProvider>
-    <RouterProvider router={router} />
   </StrictMode>
 );
