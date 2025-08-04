5f2386d346763e797042386c9099101190b669b5
Saagar-Poudel <poudelsagar920@gmail.com>
2025-08-04 16:43:57 +0545

    changing files roots

--- frontend/src/Component/Home/Home.jsx
+++ frontend/src/Component/Home/Home.jsx
@@ -3,7 +3,7 @@ import Hero from './Hero';
 import ReviewSection from './Review';
 import WeatherAndPrices from '../WeatherAndPrices';
 import NewsSection from '../NewsSection';
-import Marketplace from '../Marketplace';
+import Marketplace from './Marketplace';
 
 const Home = () => {
   return (
