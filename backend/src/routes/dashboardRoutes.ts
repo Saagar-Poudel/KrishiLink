ff754a8fa3affa5fa608f58448c4188d6179a012
Saagar-Poudel <poudelsagar920@gmail.com>
2025-11-10 11:34:12 +0545

    updating

--- backend/src/routes/dashboardRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getSummary } from "../controllers/dashboardController";
-
-const router = Router();
-
-// Define your dashboard routes here
-router.get('/summary', getSummary);
-
-export default router;
--- backend/src/routes/newsRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { getNepalAgriNews } from "../controllers/newsController";
-
-const router = Router();
-router.get("/", getNepalAgriNews);
-
-export default router;
